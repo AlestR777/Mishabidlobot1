f9efd91d4688f3ff1560ff39a9b910389fa60147
AlestR777 <yanedaun22@gmail.com>
2026-01-27 13:36:10 +1000

    Update botn.py

--- botn.py
+++ botn.py
@@ -10,7 +10,7 @@ TELEGRAM_TOKEN = "8535045078:AAEJvCN6TKdlgkTFZ0j0HNEUCzYVvQ5ulc8"
 OPENROUTER_API_KEY = "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"
 
 # Настройки модели
-MODEL_NAME = "xiaomi/mimo-v2-flash:free"
+MODEL_NAME = "tngtech/deepseek-r1t2-chimera:free"
 RANDOM_ANSWER_CHANCE = 0.1 
 
 # УСИЛЕННЫЙ СИСТЕМНЫЙ ПРОМПТ (Jailbreak style)
